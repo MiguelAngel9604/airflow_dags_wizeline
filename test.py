0e9707d28a9496696db8d13f1e2616f5eef13d61
Miguel Angel <63972784+MiguelAngel9604@users.noreply.github.com>
2022-07-14 14:22:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.dummy import DummyOperator
-
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
 
 DAG_ID = "gcp_database_ingestion_workflow"
 CLOUD_PROVIDER = "gcp"
 STABILITY_STATE = "unstable"
+GCP_CONN_ID = "google_cloud_default"
+GCS_BUCKET_NAME = "wizeline-project-356123-input"
+GCS_KEY_NAME = "user_purchase.csv"
 
 with DAG(
     dag_id = DAG_ID,
@@ -15,4 +18,13 @@ with DAG(
 ) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
     
+    verify_key_existence = GCSObjectExistenceSensor(
+        task_id="verify_key_existence",
+        google_cloud_conn_id=GCP_CONN_ID,
+        bucket=GCS_BUCKET_NAME,
+        object=GCS_KEY_NAME,
+    )
+    (
     start_workflow
+    >> verify_key_existence
+    )
