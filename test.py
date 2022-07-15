9b657641ae24345105a20f852c467de490f2dbb7
Miguel Angel <63972784+MiguelAngel9604@users.noreply.github.com>
2022-07-14 19:20:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -33,12 +33,13 @@ def ingest_data_from_gcs (
     
     gcs_hook = GCSHook(gcp_conn_id=gcp_conn_id)
     psql_hook = PostgresHook(postgres_conn_id)
-    
+
     with tempfile.NamedTemporaryFile() as tmp:
         gcs_hook.download(
-            bucket_name=gcs_bucket, object_name=gcs_object,filename=tmp.name
+            bucket_name=gcs_bucket, object_name=gcs_object, filename=tmp.name
         )
         psql_hook.bulk_load(table=postgres_table, tmp_file=tmp.name)
+
 
 with DAG(
     dag_id = DAG_ID,
