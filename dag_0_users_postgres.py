affa8e461c8f0d744ec087a7fa8d27bfb9203f35
Miguel Angel <63972784+MiguelAngel9604@users.noreply.github.com>
2022-08-01 21:23:32 -0500

    Update dag_0_users_postgres.py

--- dag_0_users_postgres.py
+++ dag_0_users_postgres.py
@@ -9,7 +9,7 @@ from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.sql import BranchSQLOperator
 
-DAG_ID = "users_postgres"
+DAG_ID = 'dag_0_users_postgres'
 CLOUD_PROVIDER = "gcp"
 STABILITY_STATE = "unstable"
 
