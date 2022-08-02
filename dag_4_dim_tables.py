9d42638d794cbe55e89777b4f146abf32e36abef
Miguel Angel <63972784+MiguelAngel9604@users.noreply.github.com>
2022-08-01 21:59:33 -0500

    Update dag_4_dim_tables.py

--- dag_4_dim_tables.py
+++ dag_4_dim_tables.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from datetime import datetime
+from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.dataproc import (
     DataprocCreateClusterOperator,
     DataprocSubmitJobOperator,
